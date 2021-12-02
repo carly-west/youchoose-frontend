2591702a86b02caf258c8a7883b6fc26eda3c0c0
carly-west <carly.west99@gmail.com>
2021-12-01 19:49:28 -0700

    done?

--- js/verifyAccess.js
+++ js/verifyAccess.js
@@ -1,4 +1,4 @@
-import { getSessionStorage } from "./utils.js";
+import { getSessionStorage, qs } from "./utils.js";
 
 function verifyAccess() {
   const token = getSessionStorage("userToken");
@@ -6,20 +6,24 @@ function verifyAccess() {
     location.href = "/index.html";
     qs(".admin-dropdown").classList.add("hidden");
   }
+  else {
+    // This was the only way I could get the join to work. It throws an error, but there is probably a better way to do this, but I don't fully understand.
+    continue;
+  }
 }
 
 verifyAccess();
 
+console.log(getLocalStorage("userToken"));
 
 
+const logoutButton = document.getElementById("logout-button");
 
-// const logoutButton = document.getElementById("logout-button");
-
-// logoutButton.addEventListener("touchend", function (e) {
-//   sessionStorage.removeItem("userToken");
-//   sessionStorage.removeItem("userId");
-// });
-// logoutButton.addEventListener("click", function (e) {
-//   sessionStorage.removeItem("userToken");
-//   sessionStorage.removeItem("userId");
-// });
+logoutButton.addEventListener("touchend", function (e) {
+  sessionStorage.removeItem("userToken");
+  sessionStorage.removeItem("userId");
+});
+logoutButton.addEventListener("click", function (e) {
+  sessionStorage.removeItem("userToken");
+  sessionStorage.removeItem("userId");
+});
