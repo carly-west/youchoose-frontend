48aed28538ac82654774701c930a6340535056af
carly-west <carly.west99@gmail.com>
2021-12-01 19:36:57 -0700

    changes

--- js/verifyAccess.js
+++ js/verifyAccess.js
@@ -9,3 +9,17 @@ function verifyAccess() {
 }
 
 verifyAccess();
+
+
+
+
+// const logoutButton = document.getElementById("logout-button");
+
+// logoutButton.addEventListener("touchend", function (e) {
+//   sessionStorage.removeItem("userToken");
+//   sessionStorage.removeItem("userId");
+// });
+// logoutButton.addEventListener("click", function (e) {
+//   sessionStorage.removeItem("userToken");
+//   sessionStorage.removeItem("userId");
+// });
